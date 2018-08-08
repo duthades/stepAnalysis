4c93e47d065d41276cde72a321baa47529f8d476
duthades <duthades@gmail.com>
2018-08-08 11:05:22 +0530

    Add project

---
+++ main_data.py
@@ -0,0 +1,45 @@
+"""This module gives the analysis of the route from home to office"""
+import os
+import math
+import pandas as pd
+
+
+def find_peaks(acc_):
+    """Find peaks in the series"""
+    peaks = 0
+    for i in range(1, len(acc_)-1):
+        if (acc_[i - 1] < acc_[i] and acc_[i] > acc_[i + 1]):
+            peaks += 1
+    return peaks
+
+
+DISTANCE = 900
+
+DATA = pd.read_csv('1B_6th.csv', sep=';', header=1)
+
+HEADER = DATA.columns.values.tolist()
+DATA = DATA.set_index(HEADER[-2])
+
+DATA.index = pd.to_datetime(DATA.index, unit='ms')
+
+ACCX = DATA[HEADER[6]]
+ACCY = DATA[HEADER[7]]
+ACCZ = DATA[HEADER[8]]
+ACC = ACCX ** 2 + ACCY ** 2 + ACCZ ** 2
+ACC = ACC.apply(math.sqrt)  # magnitude of Acceleration
+ACC = (ACC - ACC.mean())/ACC.std()  # Normalize Acceleration
+
+TIME_SEC = (DATA.index[-1] - DATA.index[0]).total_seconds()
+
+NO_OF_STEPS = find_peaks(-1 * ACC) + find_peaks(ACC)
+STEPS_PER_MIN = NO_OF_STEPS / (TIME_SEC / 60)
+AVERAGE_STRIDE_LENGTH = DISTANCE / NO_OF_STEPS
+TIME = TIME_SEC / 60
+
+os.system('clear')
+
+print("The distance form PG to 1B office is %d m" % DISTANCE)
+print("Total steps taken to reach the destination = %d" % NO_OF_STEPS)
+print("The number of steps taken per min = %d" % STEPS_PER_MIN)
+print("Average stride length = %.2f m" % AVERAGE_STRIDE_LENGTH)
+print("Total time to reach destination = %.2f min" % TIME)
